8edf02faaeb930ee93fe04097eedfab02b61d154
Pawan Chaudhary <chaudharypawan18@yahoo.com>
2025-06-12 08:27:26 +0530

    everything is good before revise starting

--- frontend/src/components/productcard.js
+++ frontend/src/components/productcard.js
@@ -5,32 +5,29 @@ import Rating from "../components/rating.js";
 function ProductCard({ product }) {
   return (
     <div className="col-md-4 mb-5">
-      <div className="card h-100">
-        <img
-          src={product.image}
-          alt={product.name}
-          className="card-img-top"
-          style={{ height: "300px", objectFit: "cover", width: "auto" }}
-        />
-        <div className="card-body d-flex flex-column">
-          <h5> {product.name}</h5>
-          <p>
-            {product.price} |{" "}
-            <span>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              ></Rating>
-            </span>
-          </p>
-          <Link
-            to={`product/${product._id}`}
-            className="btn btn-outline-success"
-          >
-            Add To Cart
-          </Link>
+      <Link to={`product/${product._id}`}>
+        <div className="card h-100">
+          <img
+            src={product.image}
+            alt={product.name}
+            className="card-img-top"
+            style={{ height: "300px", objectFit: "cover", width: "auto" }}
+          />
+          <div className="card-body d-flex flex-column">
+            <h5> {product.name}</h5>
+            <p>
+              ${product.price} |{" "}
+              <span>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                ></Rating>
+              </span>
+            </p>
+            <Link to={`product/${product._id}`}></Link>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
