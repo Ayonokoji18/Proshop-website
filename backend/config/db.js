a63467b931d61aae040eaa1035706fcd9c768adc
Pawan Chaudhary <chaudharypawan18@yahoo.com>
2025-06-21 23:30:12 +0530

    So Finally The Rise of Shield Hero finally finished

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,13 @@ dotenv.config();
 
 async function connectDB() {
   try {
-    const conn = mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDb is Connected ${conn.connection.host}`);
-    console.log(`Mongo Uri ${process.env.MONGO_URI}`);
-  } catch (error) {
-    console.log(`Error, ${error.message}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connection is connect");
+    process.exit();
+  } catch (err) {
+    console.log(`Some Problem occured ,Mongo connection failed ${err}`);
     process.exit(1);
   }
 }
+
 export default connectDB;
